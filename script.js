f23edce9395d482048b9d7f389e3c38825017f4f
jovi <73748197+jovi920204@users.noreply.github.com>
2022-10-27 20:47:42 +0800

    create clock

---
+++ script.js
@@ -0,0 +1,28 @@
+function showTime(){
+    let date = new Date();
+        dataValues = [
+        date.getFullYear(),
+        date.getMonth() + 1,
+        date.getDate(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds(),
+    ];
+    var hour12;
+    var hour24;
+    if (date.getHours() > 12){
+        hour24 = date.getHours();
+        hour12 = hour24-12;
+    }
+    else{
+        hour24 = hour12 = date.getHours();
+    }
+    let hour = document.getElementsByClassName("hh");
+    hour[0].textContent = String(hour12).padStart(2,'0');
+    let minute = document.getElementsByClassName("mm");
+    minute[0].textContent = String(date.getMinutes()).padStart(2,'0');
+    let second = document.getElementsByClassName("ss");
+    second[0].textContent = String(date.getSeconds()).padStart(2,'0');
+    console.log(dataValues);
+};
+setInterval((showTime),500);
